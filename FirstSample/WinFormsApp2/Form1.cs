d0ea8ec44bb3b8aab8dda10df2c649a07f3f4bd6
chiefexb <chief_exb@mail.ru>
2024-10-28 01:48:23 +0300

    1

---
+++ FirstSample/WinFormsApp2/Form1.cs
@@ -0,0 +1,16 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ FirstSample/WinFormsApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace WinFormsApp2
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+
+        static DialogResult ShowMessageBoxes()
+        {
+            //показ окна, отображающего текстовое
+            //сообщение и кнопку ОК
+            String message = "Окно, отображающее текстовое сообщение";
+            MessageBox.Show(message);
+            //показ окна с текстом и двумя кнопками
+            //OK и CANCEL
+            message = "Окно с текстом и двумя кнопками OK и CANCEL";
+            String caption = "Окно с двумя кнопками";
+            DialogResult result = MessageBox.
+            Show(message, caption,
+            MessageBoxButtons.OKCancel);
+            String button = result.ToString();
+        
+    
+    //показ Окна с тремя кнопками и какой-то
+    //иконкой
+    result = MessageBox.Show("Вы нажали кнопку"+ button+ ". Повторить?",button, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Asterisk);
+        return result;
+        }
+         [STAThread]
+    static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            //ApplicationConfiguration.Initialize();
+            DialogResult result;
+            do
+            {
+                result = ShowMessageBoxes();
+            } while (result == DialogResult.Retry);
+           // Application.Run(new Form1());
+
+
+
+
+        }
+    }
+}
