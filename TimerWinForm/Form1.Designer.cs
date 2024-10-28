6902938ad2edec14ffe2a60111d2fd1ab8b7844d
chiefexb <chief_exb@mail.ru>
2024-10-28 13:57:46 +0300

    1

---
+++ TimerWinForm/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace TimerWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            numericUpDown1 = new NumericUpDown();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(592, 111);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 0;
+            numericUpDown1.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // button1
+            // 
+            button1.Location = new Point(309, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Старт";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(415, 111);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "СТОП";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private NumericUpDown numericUpDown1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ TimerWinForm/Form1.cs
@@ -0,0 +1,38 @@
+﻿namespace TimerWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button2.Enabled = false;
+            MessageBox.Show("Таймер отработал!", "Таймер");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value <= 0)
+            {
+                MessageBox.Show("Количество секунд                 должно быть больше 0!");
+            return;
+            }
+            //
+            button2.Enabled = true;
+            timer1.Interval = Decimal.ToInt32(numericUpDown1.Value) * 1000;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("Таймер не успел             отработать!", "Таймер");
+            button2.Enabled = false;
+        }
+    }
+}
