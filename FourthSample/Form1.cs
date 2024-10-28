de487ba1f69f9b1c5bef040008d16768125f2863
chiefexb <chief_exb@mail.ru>
2024-10-28 16:55:54 +0300

    1

---
+++ FourthSample/Form1.cs
@@ -0,0 +1,26 @@
+﻿namespace FourthSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = DateTime.Now.ToLongTimeString();
+            //закрепление обработчика
+            //timer1.Tick += new EventHandler(ShowTime);
+            //установка интервала времени
+            timer1.Interval = 500;
+            timer1.Start();
+        
+           
+
+    
+    //стартуем таймер
+}
+
+    private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
---
+++ task3/Form1.cs
@@ -0,0 +1,35 @@
+﻿namespace task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            String message = "";
+            if ((e.Button == MouseButtons.Right) && (ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                message = "Вы нажали правую кнопку мыши+CTRL.";
+               // ModifierKeys
+
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                message = "Вы нажали левую кнопку мыши.";
+            }
+            message += "\n" + "Координаты мыши: х=" + e.X.ToString() + ";y = " + e.Y.ToString();
+
+            //выведем сообщение в диалоговое окно
+            String caption = "Клик мыши";
+            DialogResult result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
